bcc5e6675b0013835bfadaef959d4c4fcf86440a
FrendySS <tashmit.musaev@gmail.com>
2025-04-12 13:50:52 +0500

    Trigger pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,9 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.monitor)
+    implementation(libs.androidx.junit.ktx)
+    testImplementation ("junit:junit:4.13.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
